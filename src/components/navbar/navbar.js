e093440edd497449eabd41de694be80b9edb0ff7
Noah Strayve <noahstrayve@gmail.com>
2018-08-22 09:40:30 -0500

    last pushes of yesterday

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,25 +5,33 @@ import { Link } from "react-router-dom";
 const Header = props => {
 	return <div className="headerContainer">
 				<div className="logoContainer">
-					<h2>Plai-It</h2>
+					<h2 id="logoTextOne">P</h2>
+					<h2 id="logoTextTwo">L</h2>
+					<h2 id="logoTextThree">A</h2>
+					<h2 id="logoTextFour">I</h2>
+					<h2 id="logoTextFive">-</h2>
+					<h2 id="logoTextSix">I</h2>
+					<h2 id="logoTextSeven">T</h2>
 				</div>
-				<div className="linkContainerTop">
-					<div className="linkBall">
-						<p id="linkText">Home</p>
+				<div className="linkContainer">
+					<div className="linkContainerTop">
+						<div className="linkBall">
+							<p id="linkText">Home</p>
+						</div>
+						<div className="linkBall">
+							<p id="linkText">Events</p>
+						</div>
+						<div className="linkBall">
+							<p id="linkText">Contact</p>
+						</div>
 					</div>
-					<div className="linkBall">
-						<p id="linkText">Events</p>
-					</div>
-					<div className="linkBall">
-						<p id="linkText">Contact</p>
-					</div>
-				</div>
-				<div className="linkContainerBottom">
-					<div className="linkBall">
-						<p id="linkText">Store</p>
-					</div>
-					<div className="linkBall">
-						<p id="linkText">News</p>
+					<div className="linkContainerBottom">
+						<div className="linkBall">
+							<p id="linkText">Store</p>
+						</div>
+						<div className="linkBall">
+							<p id="linkText">News</p>
+						</div>
 					</div>
 				</div>
 			</div>;
